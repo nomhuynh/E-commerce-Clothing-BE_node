4560a98ace0a3c918ded534ef5cfbc2662cf4c7d
Nam <namhuynh.101104@gmail.com>
2026-01-24 21:59:18 +0700

    add api register and login

--- src/config/config.js
+++ src/config/config.js
@@ -5,10 +5,14 @@ const config = {
     port: process.env.PORT,
     db: {
         host: process.env.DB_HOST,
-        user: process.env.DB_USER ,
-        password: process.env.DB_PASSWORD ,
-        name: process.env.DB_NAME 
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        name: process.env.DB_NAME
     },
+    jwt: {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES_IN || '1d',
+    }
 };
 
 module.exports = config;
