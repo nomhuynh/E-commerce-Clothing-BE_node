68401971f4879d61f6a11ec3d022596c4318a520
Nam Huynh <120807554+nomhuynh@users.noreply.github.com>
2026-02-09 13:19:23 +0700

    Delete tests directory

--- tests/utils/mockAuth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    verifyToken: jest.fn((req, res, next) => {
-        req.user = { user_id: 'admin-id', role: 'ADMIN' };
-        next();
-    }),
-    authorize: jest.fn((...roles) => (req, res, next) => next())
-};
